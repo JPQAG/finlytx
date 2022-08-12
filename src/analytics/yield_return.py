f4fa3138a46326a5b1187966bf4b4a62fdfb04e5
JPQAG <janthonyprice@gmail.com>
2022-08-13 08:12:31 +1000

    Add yield return files

---
+++ src/analytics/yield_return.py
@@ -0,0 +1,17 @@
+from typing import Dict
+
+
+class YieldReturn:
+    @staticmethod
+    def discounted_cash_flow(Input: str) -> Dict:
+        """_summary_
+
+        Args:
+            Input (str): _description_
+
+        Returns:
+            Dict: _description_
+        """
+        return {}
+
+    
---
+++ tests/analytics/test_yield_return.py
@@ -0,0 +1,14 @@
+import unittest
+
+from analytics.yield_return import YieldReturn
+
+
+class TestYieldReturn(unittest.TestCase):
+    def test_discounted_cash_flow(self):
+        returnValue = YieldReturn.discounted_cash_flow("input string")
+
+        assert type(returnValue) is dict
+        
+    # TODO
+    ## Get yield from current value, future value and cashflows.
+    ## Get

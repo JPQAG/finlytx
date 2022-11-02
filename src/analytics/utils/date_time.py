17aea3ff6ce29ebfa96f27d38e7938c826f076c6
jplytx <james@bondlytx.com>
2022-11-02 14:55:18 +1100

    Finish date_time date range generator

--- src/analytics/utils/date_time.py
+++ src/analytics/utils/date_time.py
@@ -1,5 +1,6 @@
 import datetime
 from os import stat
+from queue import Empty
 from typing import Any, List
 import pandas as pd
 from pandas.tseries.offsets import DateOffset
@@ -45,7 +46,7 @@ def years_between_dates(first_date: datetime.datetime, second_date: datetime.dat
 def generate_date_range(
     start_date: datetime.datetime, 
     end_date: datetime.datetime, 
-    freq_input: TIMESERIES_TIME_PERIODS.keys(),
+    freq_input: TIMESERIES_TIME_PERIODS.keys()="A",
     arrears: bool = True
     ) -> List:
     """_summary_
@@ -59,55 +60,112 @@ def generate_date_range(
     Returns:
         List: _description_
     """
+    assert isinstance(start_date, datetime.datetime), f"start_date must be of type datetime.datetime"
+    assert isinstance(end_date, datetime.datetime), f"end_date must be of type datetime.datetime"
+    assert start_date <= end_date, f"start_date is after end_date"
     assert freq_input in TIMESERIES_TIME_PERIODS.keys(), f"'{freq_input}' is not in {TIMESERIES_TIME_PERIODS.keys()}"
-    assert start_date <= end_date, f"'{start_date}' is after {end_date}"
-    
-    if freq_input == "A":
-        date_range = _annual_range(start_date, end_date, freq_input)
-    else: 
-        date_range = _monthly_range(start_date, end_date, freq_input)
 
-    return_list = date_range.to_native_types().tolist()[1:] if arrears else date_range[:-1].to_native_types().tolist()
+    if freq_input == "A":
+        date_range = _annual_range(start_date, end_date)
+    else: 
+        date_range = _n_monthly_range(start_date, end_date, freq_input)
+
+    range_as_list = date_range.to_native_types().tolist()
+
+    return_list = range_as_list[1:] if arrears else range_as_list[:-1]
 
     return  return_list
 
 def _annual_range(
-    start_date,
-    end_date,
-    freq
+    start_date: datetime.datetime,
+    end_date: datetime.datetime
 ) -> List:
+    assert isinstance(start_date, datetime.datetime), f"start_date must be of type datetime.datetime"
+    assert isinstance(end_date, datetime.datetime), f"end_date must be of type datetime.datetime"
+    assert start_date <= end_date, f"start_date is after end_date"
+
     return pd.date_range(start_date, end_date, freq=DateOffset(years=1))
 
-def _monthly_range(
+def _n_monthly_range(
     start_date: datetime.datetime,
     end_date: datetime.datetime,
     freq: TIMESERIES_TIME_PERIODS.keys()
 ) -> List:
     assert freq in TIMESERIES_TIME_PERIODS.keys(), f"'{freq}' is not in {TIMESERIES_TIME_PERIODS.keys}"
     assert start_date <= end_date, f"'{start_date}' is after {end_date}"
-    
-    day_of_month = datetime.datetime.strptime(start_date, "%Y-%m-%d").day
-    annual_freq = TIMESERIES_TIME_PERIODS[freq]["annual_frequency"]
-    monthly_interval = int(12 / annual_freq)
 
-    date_range_start_of_month = pd.date_range(
-        pd.Timestamp(start_date)-pd.offsets.MonthBegin(),
+    monthly_range = _monthly_range(start_date, end_date)
+    n_monthly_range = _get_n_monthly(monthly_range, freq)
+
+    return n_monthly_range
+
+def _monthly_range(
+    start_date: datetime.datetime,
+    end_date: datetime.datetime,
+) -> pd.DatetimeIndex:
+    assert isinstance(start_date, datetime.datetime), f"start_date must be of type datetime.datetime"
+    assert isinstance(end_date, datetime.datetime), f"end_date must be of type datetime.datetime"
+    assert start_date <= end_date, f"start_date is after end_date"
+    
+    day_of_month = start_date.day
+    is_start_of_month = day_of_month == 1
+
+    date_range_month_start = _generate_pd_date_range(
+        (start_date - pd.offsets.MonthBegin()) if not is_start_of_month else start_date,
         end_date,
         freq='MS'
     )
 
-    return_index = date_range_start_of_month if day_of_month == 1 else _mid_month_adj(start_date, date_range_start_of_month)
+    return_index = date_range_month_start if day_of_month == 1 else _mid_month_adj(start_date, date_range_month_start)
 
-    filtered = return_index.to_series().iloc[::monthly_interval]
+    return return_index
 
-    return pd.DatetimeIndex(filtered)
+def _generate_pd_date_range(
+    start_date: datetime.datetime,
+    end_date: datetime.datetime,
+    freq: str,
+    inclusive: str="both"
+) -> pd.DatetimeIndex:
+    return_index = pd.date_range(
+        start_date,
+        end_date,
+        freq=freq,
+        inclusive=inclusive
+    )
+
+    return return_index
 
 def _mid_month_adj(
     start_date: datetime.datetime,
     date_range: pd.DatetimeIndex
 ) -> pd.DatetimeIndex:
+    assert isinstance(date_range, pd.DatetimeIndex), f"'{date_range}' is not of type pd.DatetimeIndex."
+    assert isinstance(start_date, datetime.datetime), f"'{start_date}' is not of type datetime.datetime."
+    
     day_offset: int = pd.Timestamp(start_date).day - 1
     offset: pd.offsets.Day = pd.offsets.Day(day_offset)
     date_range_day_adjusted: pd.Series = (date_range + offset).to_series()
     date_range_day_adjusted.loc[date_range_day_adjusted.dt.month > date_range.month] -= pd.offsets.MonthEnd(1)
+
     return pd.DatetimeIndex(date_range_day_adjusted)
+
+def _get_n_monthly(
+    date_list: List[datetime.datetime],
+    freq: TIMESERIES_TIME_PERIODS.keys()
+) -> List[datetime.datetime]:
+    assert len(date_list) != 0, f"'{date_list}' is empty."
+    assert all(isinstance(date, (datetime.datetime)) for date in date_list), f"{date_list} elements must be of type datetime.datetime."
+    assert freq in TIMESERIES_TIME_PERIODS.keys(), f"{freq} is not in {TIMESERIES_TIME_PERIODS.keys()}"
+
+    date_interval = int(12/TIMESERIES_TIME_PERIODS[freq]["annual_frequency"])
+
+    filtered_list = date_list[0::date_interval]
+
+    return filtered_list
+
+def _default_date(
+    date: str
+) -> datetime.datetime:
+    assert isinstance(date, str), f"'{date}' is not of type string."
+
+    return datetime.datetime.strptime(date, "%Y-%m-%d")
--- tests/analytics/utils/test_date_time.py
+++ tests/analytics/utils/test_date_time.py
@@ -1,13 +1,20 @@
 import unittest
 from datetime import datetime
+import pandas as pd
 
 from src.analytics.utils.date_time import (
+    _annual_range,
+    _default_date,
+    _generate_pd_date_range,
+    _get_n_monthly,
+    _mid_month_adj,
     _monthly_range,
     generate_date_range,
     months_between_dates,
     years_between_dates,
     days_between_dates
 )
+from src.analytics.utils.lookup import TIMESERIES_TIME_PERIODS
 
 
 class DateTimeTestCase(unittest.TestCase):
@@ -34,46 +41,338 @@ class DateTimeTestCase(unittest.TestCase):
 class DateTimeRangeTestCase(unittest.TestCase):
 
     def test_generate_date_range(self):
-
         # Test Cases 
         ## In advance, Annual
-        self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2002-01-01", freq_input="A", arrears=False), ["2000-01-01","2001-01-01"])
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2002-01-01"), freq_input="A", arrears=False),
+            ["2000-01-01","2001-01-01"]
+        )
         ## In arrears, Annual
-        self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2002-01-01", freq_input="A", arrears=True), ["2001-01-01","2002-01-01"])
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2002-01-01"), freq_input="A", arrears=True),
+            ["2001-01-01","2002-01-01"]
+        )
+        # self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2002-01-01", freq_input="A", arrears=True), ["2001-01-01","2002-01-01"])
 
     def test_generate_date_range_start_end(self):
         # Cases
         # Start Date late
-        self.assertEqual(generate_date_range(start_date="2000-01-02", end_date="2003-01-01", freq_input="A", arrears=True), ["2001-01-02","2002-01-02"])
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-02"), end_date=_default_date("2003-01-01"), freq_input="A", arrears=True),
+            ["2001-01-02","2002-01-02"]
+        )
         # End Date late
-        self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2003-01-02", freq_input="A", arrears=True), ["2001-01-01","2002-01-01","2003-01-01"])
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2003-01-01"), freq_input="A", arrears=True),
+            ["2001-01-01","2002-01-01","2003-01-01"]
+        )
         # Start equals end
-        self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2001-01-01", freq_input="A", arrears=False), ["2000-01-01"])
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2000-01-01"), freq_input="A", arrears=True),
+            []
+        )
         # Start after end
         with self.assertRaises(Exception) as context:
             generate_date_range(start_date="2003-01-01", end_date="2000-01-01")
-            self.assertTrue("start_date after end_date" in context.exception)
+        self.assertTrue(context.exception.args[0], "start_date is after end_date")
 
     def test_generate_date_range_freq(self):
         ## Annual
-        # self.assertEqual(generate_date_range(start_date="2000-01-10", end_date="2003-01-10", freq_input="A", arrears=True), ["2001-01-10","2002-01-10","2003-01-10"])
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-10"), end_date=_default_date("2003-01-10"), freq_input="A", arrears=True),
+            ["2001-01-10","2002-01-10","2003-01-10"]
+        )
         ## Semi-Annual
-        # self.assertEqual(generate_date_range(start_date="2000-01-10", end_date="2002-01-10", freq_input="SA", arrears=True), ["2000-07-10","2001-01-10","2001-07-10","2002-01-10"])
-        ## Semi-Annual
-        self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2002-01-01", freq_input="SA", arrears=True), ["2000-07-01","2001-01-01","2001-07-01","2002-01-01"])
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2002-01-01"), freq_input="SA", arrears=True),
+            ["2000-07-01","2001-01-01","2001-07-01","2002-01-01"]
+        )
         ## Quarterly
-        # self.assertEqual(generate_date_range(start_date="2000-01-10", end_date="2002-01-10", freq_input="Q", arrears=True), ["2000-04-10","2000-07-10","2000-10-10","2001-01-10","2001-04-10","2001-07-10","2001-10-10","2002-01-10"])
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-10"), end_date=_default_date("2002-01-10"), freq_input="Q", arrears=True),
+            ["2000-04-10","2000-07-10","2000-10-10","2001-01-10","2001-04-10","2001-07-10","2001-10-10","2002-01-10"]
+        )
         ## Monthly
-        # self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2000-06-01", freq_input="M", arrears=True), ["2000-02-01","2000-03-01","2000-04-01","2000-05-01","2000-06-01"])
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2000-06-01"), freq_input="M", arrears=True),
+            ["2000-02-01","2000-03-01","2000-04-01","2000-05-01","2000-06-01"]
+        )
 
     def test_generate_date_range_arrears(self):
-        
-        self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2003-01-01", freq_input="A", arrears=True), ["2001-01-01","2002-01-01","2003-01-01"])
-        self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2001-01-01", freq_input="A", arrears=False), ["2000-01-01","2001-01-01", "2002-01-01"])
 
-        self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2000-01-04", freq_input="M", arrears=True), ["2000-02-01","2000-03-01","2000-04-01"])
-        self.assertEqual(generate_date_range(start_date="2000-01-01", end_date="2000-01-04", freq_input="M", arrears=False), ["2000-01-01","2000-02-01","2000-03-01"])
+        with self.assertRaises(Exception) as context:
+            generate_date_range(
+                _default_date("2001-01-01"),
+                _default_date("2000-01-01"),
+                "A"
+            )
+        self.assertEqual(context.exception.args[0], "start_date is after end_date")
+
+        with self.assertRaises(Exception) as context:
+            generate_date_range(
+                "2000-01-01",
+                _default_date("2001-01-01")
+            )
+        self.assertEqual(context.exception.args[0], "start_date must be of type datetime.datetime")
+
+        with self.assertRaises(Exception) as context:
+            generate_date_range(
+                _default_date("2001-01-01"),
+                "2000-01-01"
+            )
+        self.assertEqual(context.exception.args[0], "end_date must be of type datetime.datetime")
+
+        with self.assertRaises(Exception) as context:
+            generate_date_range(
+                _default_date("2001-01-01"),
+                "2000-01-01"
+            )
+        self.assertEqual(context.exception.args[0], "end_date must be of type datetime.datetime")
+
+        with self.assertRaises(Exception) as context:
+            generate_date_range(
+                _default_date("2001-01-01"),
+                "2000-01-01"
+            )
+        self.assertEqual(context.exception.args[0], "end_date must be of type datetime.datetime")
+
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2003-01-01"), freq_input="A", arrears=True),
+            ["2001-01-01","2002-01-01","2003-01-01"]
+        )
+
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2003-01-01"), freq_input="A", arrears=False),
+            ["2000-01-01","2001-01-01", "2002-01-01"]
+        )
+
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2000-04-01"), freq_input="M", arrears=False),
+            ["2000-01-01","2000-02-01","2000-03-01"]
+        )
+
+        self.assertListEqual(
+            generate_date_range(start_date=_default_date("2000-01-01"), end_date=_default_date("2000-04-01"), freq_input="M", arrears=True),
+            ["2000-02-01","2000-03-01","2000-04-01"]
+        )
+
+    def test_annual_range(self):
+
+        with self.assertRaises(Exception) as context:
+            _annual_range(
+                _default_date("2001-01-01"),
+                _default_date("2000-01-01")
+            )
+        self.assertEqual(context.exception.args[0], "start_date is after end_date")
+
+        with self.assertRaises(Exception) as context:
+            _annual_range(
+                "2000-01-01",
+                _default_date("2001-01-01")
+            )
+        self.assertEqual(context.exception.args[0], "start_date must be of type datetime.datetime")
+
+        with self.assertRaises(Exception) as context:
+            _annual_range(
+                _default_date("2001-01-01"),
+                "2000-01-01"
+            )
+        self.assertEqual(context.exception.args[0], "end_date must be of type datetime.datetime")
+
+        pd.testing.assert_index_equal(
+            _annual_range(_default_date("2000-01-01"), _default_date("2000-01-01")),
+            pd.DatetimeIndex(["2000-01-01"])
+        )
+
+        pd.testing.assert_index_equal(
+            _annual_range(_default_date("2000-01-01"), _default_date("2000-01-01")),
+            pd.DatetimeIndex(["2000-01-01"])
+        )
+
+        pd.testing.assert_index_equal(
+            _annual_range(_default_date("2000-01-01"), _default_date("2003-01-01")),
+            pd.DatetimeIndex(["2000-01-01", "2001-01-01", "2002-01-01", "2003-01-01"])
+        )
 
     def test_monthly_range(self):
 
-        self.assertEqual(_monthly_range(start_date="2000"))
+        with self.assertRaises(Exception) as context:
+            _monthly_range(
+                _default_date("2001-01-01"),
+                _default_date("2000-01-01")
+            )
+        self.assertEqual(context.exception.args[0], "start_date is after end_date")
+
+        with self.assertRaises(Exception) as context:
+            _monthly_range(
+                "2000-01-01",
+                _default_date("2001-01-01")
+            )
+        self.assertEqual(context.exception.args[0], "start_date must be of type datetime.datetime")
+
+        with self.assertRaises(Exception) as context:
+            _monthly_range(
+                _default_date("2001-01-01"),
+                "2000-01-01"
+            )
+        self.assertEqual(context.exception.args[0], "end_date must be of type datetime.datetime")
+
+        pd.testing.assert_index_equal(
+            _monthly_range(_default_date("2000-01-01"), _default_date("2000-03-01")).strftime("%Y-%m-%d"), 
+            pd.DatetimeIndex(["2000-01-01", "2000-02-01", "2000-03-01"]).strftime("%Y-%m-%d")
+        )
+
+        pd.testing.assert_index_equal(
+            _monthly_range(_default_date("2000-01-10"), _default_date("2000-03-10")).strftime("%Y-%m-%d"), 
+            pd.DatetimeIndex(["2000-01-10", "2000-02-10", "2000-03-10"]).strftime("%Y-%m-%d")
+        )
+
+    def test_mid_month_adj(self):
+
+        with self.assertRaises(Exception) as context:
+            _mid_month_adj(
+                _default_date("2000-01-01"),
+                None
+            )
+        self.assertEqual(context.exception.args[0], "'None' is not of type pd.DatetimeIndex.")
+
+        with self.assertRaises(Exception) as context:
+            _mid_month_adj(
+                "2000-01-01",
+                pd.date_range("2000-01-01", "2001-01-01")
+            )
+        self.assertEqual(context.exception.args[0], "'2000-01-01' is not of type datetime.datetime.")
+
+        self.assertCountEqual(
+            _mid_month_adj(_default_date("2000-01-20"), pd.date_range("2000-01-01", "2000-03-01", freq="MS")).strftime("%Y-%m-%d"),
+            pd.DatetimeIndex(["2000-01-20", "2000-02-20", "2000-03-20"]).strftime("%Y-%m-%d")
+        )
+
+        self.assertCountEqual(
+            _mid_month_adj(_default_date("2000-01-01"), pd.date_range("2000-01-01", "2000-03-01", freq="MS")).strftime("%Y-%m-%d"),
+            pd.DatetimeIndex(["2000-01-01", "2000-02-01", "2000-03-01"]).strftime("%Y-%m-%d")
+        )
+
+    def test_pd_generate_date_range(self):
+
+        pd.testing.assert_index_equal(
+            _generate_pd_date_range(_default_date("2000-01-01"), _default_date("2000-03-01"), 'MS'),
+            pd.DatetimeIndex(["2000-01-01", "2000-02-01", "2000-03-01"], dtype='datetime64[ns]', freq=None)
+        )
+
+        pd.testing.assert_index_equal(
+            _generate_pd_date_range(_default_date("2000-01-10") - pd.offsets.MonthBegin(), _default_date("2000-03-10"), 'MS'),
+            pd.DatetimeIndex(["2000-01-01", "2000-02-01", "2000-03-01"], dtype='datetime64[ns]', freq=None)
+        )
+
+    def test_get_n_monthly(self):
+
+        # Empty
+        with self.assertRaises(Exception) as context:
+            _get_n_monthly([], "A")
+        # Non datetime.datetime
+        with self.assertRaises(Exception) as context:
+            _get_n_monthly(["2000-01-01"], "A")
+
+        # Semi-Annual
+        self.assertEqual(
+            _get_n_monthly(
+                [
+                    datetime.strptime("2000-01-01", "%Y-%m-%d"), 
+                    datetime.strptime("2000-02-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-03-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-04-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-05-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-06-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-07-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-08-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-09-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-10-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-11-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-12-01", "%Y-%m-%d"),
+                    datetime.strptime("2001-01-01", "%Y-%m-%d")
+                ],
+                "SA"
+            ),
+            [
+                datetime.strptime("2000-01-01", "%Y-%m-%d"),
+                datetime.strptime("2000-07-01", "%Y-%m-%d"),
+                datetime.strptime("2001-01-01", "%Y-%m-%d")
+            ]
+        )
+        # Quarterly
+        self.assertEqual(
+            _get_n_monthly(
+                [
+                    datetime.strptime("2000-01-01", "%Y-%m-%d"), 
+                    datetime.strptime("2000-02-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-03-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-04-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-05-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-06-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-07-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-08-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-09-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-10-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-11-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-12-01", "%Y-%m-%d"),
+                    datetime.strptime("2001-01-01", "%Y-%m-%d")
+                ],
+                "Q"
+            ),
+            [
+                datetime.strptime("2000-01-01", "%Y-%m-%d"), 
+                datetime.strptime("2000-04-01", "%Y-%m-%d"),
+                datetime.strptime("2000-07-01", "%Y-%m-%d"),
+                datetime.strptime("2000-10-01", "%Y-%m-%d"),
+                datetime.strptime("2001-01-01", "%Y-%m-%d")
+            ]
+        )
+        # Monthly
+        self.assertEqual(
+            _get_n_monthly(
+                [
+                    datetime.strptime("2000-01-01", "%Y-%m-%d"), 
+                    datetime.strptime("2000-02-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-03-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-04-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-05-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-06-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-07-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-08-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-09-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-10-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-11-01", "%Y-%m-%d"),
+                    datetime.strptime("2000-12-01", "%Y-%m-%d"),
+                    datetime.strptime("2001-01-01", "%Y-%m-%d")
+                ],
+                "M"
+            ),
+            [
+                datetime.strptime("2000-01-01", "%Y-%m-%d"), 
+                datetime.strptime("2000-02-01", "%Y-%m-%d"),
+                datetime.strptime("2000-03-01", "%Y-%m-%d"),
+                datetime.strptime("2000-04-01", "%Y-%m-%d"),
+                datetime.strptime("2000-05-01", "%Y-%m-%d"),
+                datetime.strptime("2000-06-01", "%Y-%m-%d"),
+                datetime.strptime("2000-07-01", "%Y-%m-%d"),
+                datetime.strptime("2000-08-01", "%Y-%m-%d"),
+                datetime.strptime("2000-09-01", "%Y-%m-%d"),
+                datetime.strptime("2000-10-01", "%Y-%m-%d"),
+                datetime.strptime("2000-11-01", "%Y-%m-%d"),
+                datetime.strptime("2000-12-01", "%Y-%m-%d"),
+                datetime.strptime("2001-01-01", "%Y-%m-%d")
+            ]
+        )
+
+    def test_default_date(self):
+
+        # Pre-checks
+        ## Is string
+        with self.assertRaises(Exception) as context:
+            _default_date(12)
+        ## Throws exception when string format not correct
+        with self.assertRaises(Exception) as context:
+            _default_date("01-01-2000")
+        
+        self.assertEqual(_default_date("2000-01-01"), datetime.strptime("2000-01-01", "%Y-%m-%d"), "Normal case failed.")
